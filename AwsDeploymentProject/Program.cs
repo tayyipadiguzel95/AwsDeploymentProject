0b2cf841f805aadbba8485fa7969892dc407d2c7
Tayyip Adıgüzel <56830252+tayyipadiguzel95@users.noreply.github.com>
2024-10-04 17:08:55 +0300

    Update Program.cs

--- AwsDeploymentProject/Program.cs
+++ AwsDeploymentProject/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
